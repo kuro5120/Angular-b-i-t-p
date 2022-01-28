4db0133aa0c8e3f6f61e805cb5a71bafe4bc851d
kuro5120 <doanpngochieu@gmail.com>
2022-01-28 19:54:56 +0700

    summary

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { AccountManagementComponent } from './account-management/account-management.component';
+import { BookingManagementComponent } from './booking-management/booking-management.component';
+import { BookingComponent } from './booking/booking.component';
+import { CardComponent } from './card/card.component';
+import { ContractManagementComponent } from './contract-management/contract-management.component';
+import { MyCardComponent } from './my-card/my-card.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 
-const routes: Routes = [{
-  path: 'sign-in',
+const routes: Routes = [
+  {
+  path: 'login',
   component: SignInComponent
-}];
+  },
+  {
+    path: 'booking',
+    component: BookingComponent
+  },
+  {
+    path: 'booking-management',
+    component: BookingManagementComponent
+  },
+  {
+    path: 'account-management',
+    component: AccountManagementComponent
+  },
+  {
+    path: 'card',
+    component: CardComponent
+  },
+  {
+    path: 'my-card',
+    component: MyCardComponent
+  },
+  {
+    path: 'contract-management',
+    component: ContractManagementComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
